61511be4a4a0f7a0b9acb59c3070d8b163b15e4e
“r4dat” <rfourtoss@gmail.com>
2015-03-04 09:04:35 -0600

    Add interp etc.

--- src/edu/washburn/DaubechiesWaveletTransform/DaubWaveTrans.java
+++ src/edu/washburn/DaubechiesWaveletTransform/DaubWaveTrans.java
@@ -8,20 +8,39 @@ import static java.lang.Math.sqrt;
  */
 public class DaubWaveTrans {
 private static final double SQRT3 = sqrt(3);
+private static final double SQRT2 = sqrt(2);
 
     public static double[] DaubWaveTrans(double[] input) {
 // This function assumes that input.length=2^n, n>1
-        double[] interp = new double[input.length];
-        double[] diff = new double[input.length];
+        int size = input.length;
+        double[] interp = new double[size];
+        double[] diff = new double[size];
         //First step interpolates between even and odd elements of the series.
-        for (int i = 0; i < (input.length)/2; i++) {
+        for (int i = 0; i < size/2; i++) {
             interp[i]=input[2*i] + SQRT3 +input[2*i+1];
             }
+        
         //Calculate difference between interpolation and actual value.
-        diff[0] = input[1]-SQRT3/4*interp[0]-(SQRT3-2)/4 * interp[input.length/2-1];
-        
-        
+        diff[0] = input[1]-SQRT3/4*interp[0]-(SQRT3-2)/4 * interp[size/2-1];
+        for(int i=1; i<size/2; i++){
+            diff[i] = input[2*i+1] - SQRT3/4*interp[i] - (SQRT3-2)/4 * interp[i-1];
         }
+        
+        //Adjust interpolations by difference. (Correction)
+        for(int i=0; i<size/2-1;i++){
+            interp[i] = interp[i] - diff[i+1];
+        }
+        interp[size/2-1] = interp[size/2-1] - diff[0];
+        
+        //Normalize vectors.
+         for(int i=1; i<size/2; i++) interp[i] = (SQRT3-1)/SQRT2 * interp[i];
+         for(int i=1; i<size/2; i++) diff[i] = (SQRT3+1)/SQRT2 * diff[i];
+         
+         double[] retarr = new double[size];
+        for(int i =0;i<size/2;i++) retarr[i]=interp[i];
+        for(int i =size/2;i<size;i++)retarr[i]=diff[i];
+        return retarr;
+         
     }
 
 }

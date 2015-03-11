91bd77f9acbf6b06ab473186b80489997664e2df
“r4dat” <rfourtoss@gmail.com>
2015-03-10 22:38:40 -0500

    Switch to looping call. Working.

--- src/edu/washburn/DaubechiesWaveletTransform/DaubWaveTrans.java
+++ src/edu/washburn/DaubechiesWaveletTransform/DaubWaveTrans.java
@@ -13,24 +13,19 @@ public class DaubWaveTrans {
     private static final double SQRT2 = sqrt(2);
 
     public static double[] DaubWaveTrans(double[] input) {
-        // This function assumes that input.length=2^n, n>1
+// This function assumes that input.length=2^n, n>1
         double[] output = new double[input.length];
-        double[] interp = new double[input.length / 2];
-        double[] diff = new double[input.length / 2];
-        
-        //Break recursive call
-        if(input.length==1){
-            output=input;
-            return output;}
-        
-            System.out.println("Input"+Arrays.toString(input));
+
+        while(input.length > 1)  {
+            double[] interp = new double[input.length / 2];
+            double[] diff = new double[input.length / 2];
             //First step does interpolation between pairs of sequential elelments.
             //1,2; 2,3; 4,5 etc.
             for (int i = 0; i < input.length / 2; i++) {
                 interp[i] = input[2 * i] + SQRT3 * input[2 * i + 1];
             }
 
-        //Calculate weighted difference between input previous 2 interp values.
+            //Calculate weighted difference between input previous 2 interp values.
             //The pre-for loop line handles index 0 by wrapping around. 
             diff[0] = input[1] - SQRT3 / 4.0 * interp[0] - (SQRT3 - 2) / 4.0 * interp[input.length / 2 - 1];
             for (int i = 1; i < input.length / 2; i++) {
@@ -41,7 +36,7 @@ public class DaubWaveTrans {
             for (int i = 0; i < input.length / 2 - 1; i++) {
                 interp[i] = interp[i] - diff[i + 1];
             }
-        //Deal with final element correct by wrapping around. 
+            //Deal with final element correct by wrapping around. 
             //Overwrites previous entry.
             interp[input.length / 2 - 1] = interp[input.length / 2 - 1] - diff[0];
 
@@ -57,12 +52,12 @@ public class DaubWaveTrans {
 //        System.out.println(Arrays.toString(diff));
             System.arraycopy(interp, 0, output, 0, input.length / 2);
             System.arraycopy(diff, 0, output, input.length / 2, input.length / 2);
-            System.out.println("Output: "+Arrays.toString(output));
-            System.out.println();
-            
-            //Swap arrays to do next iteration
-            DaubWaveTrans(interp);
-    return null;
+
+            //Set input equal to the remaining coefficients for next round.
+            input = interp;
+
+        }
+        return output;
 
     }
 }

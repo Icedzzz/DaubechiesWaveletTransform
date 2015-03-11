6a0cac4d79c8d4a744b4d13430e18bfd48341cc8
“r4dat” <rfourtoss@gmail.com>
2015-03-10 21:52:01 -0500

    Add recursive call. No exited yet.

--- src/edu/washburn/DaubechiesWaveletTransform/DaubWaveTrans.java
+++ src/edu/washburn/DaubechiesWaveletTransform/DaubWaveTrans.java
@@ -13,49 +13,56 @@ public class DaubWaveTrans {
     private static final double SQRT2 = sqrt(2);
 
     public static double[] DaubWaveTrans(double[] input) {
-// This function assumes that input.length=2^n, n>1
+        // This function assumes that input.length=2^n, n>1
         double[] output = new double[input.length];
-        double[] interp = new double[input.length/2];
-        double[] diff = new double[input.length/2];
+        double[] interp = new double[input.length / 2];
+        double[] diff = new double[input.length / 2];
         
+        //Break recursive call
+        if(input.length==1){
+            output=input;
+            return output;}
         
-        
-        //First step does interpolation between pairs of sequential elelments.
-        //1,2; 2,3; 4,5 etc.
-        for (int i = 0; i < input.length / 2; i++) {
-            interp[i] = input[2 * i] + SQRT3 * input[2 * i + 1];
-        }
+            System.out.println("Input"+Arrays.toString(input));
+            //First step does interpolation between pairs of sequential elelments.
+            //1,2; 2,3; 4,5 etc.
+            for (int i = 0; i < input.length / 2; i++) {
+                interp[i] = input[2 * i] + SQRT3 * input[2 * i + 1];
+            }
 
         //Calculate weighted difference between input previous 2 interp values.
-        //The pre-for loop line handles index 0 by wrapping around. 
-        diff[0] = input[1] - SQRT3 / 4.0 * interp[0] - (SQRT3 - 2) / 4.0 * interp[input.length / 2 - 1];
-        for (int i = 1; i < input.length / 2; i++) {
-            diff[i] = input[2 * i + 1] - SQRT3 / 4.0 * interp[i] - (SQRT3 - 2) / 4.0 * interp[i - 1];
-        }
+            //The pre-for loop line handles index 0 by wrapping around. 
+            diff[0] = input[1] - SQRT3 / 4.0 * interp[0] - (SQRT3 - 2) / 4.0 * interp[input.length / 2 - 1];
+            for (int i = 1; i < input.length / 2; i++) {
+                diff[i] = input[2 * i + 1] - SQRT3 / 4.0 * interp[i] - (SQRT3 - 2) / 4.0 * interp[i - 1];
+            }
 
-        //Adjust interpolations towards the next difference value. (Correction)
-        for (int i = 0; i < input.length / 2 - 1; i++) {
-            interp[i] = interp[i] - diff[i + 1];
-        }
+            //Adjust interpolations towards the next difference value. (Correction)
+            for (int i = 0; i < input.length / 2 - 1; i++) {
+                interp[i] = interp[i] - diff[i + 1];
+            }
         //Deal with final element correct by wrapping around. 
-        //Overwrites previous entry.
-        interp[input.length / 2 - 1] = interp[input.length / 2 - 1] - diff[0];
+            //Overwrites previous entry.
+            interp[input.length / 2 - 1] = interp[input.length / 2 - 1] - diff[0];
 
-        //Normalize vectors.
-        for (int i = 0; i < input.length / 2; i++) {
-            interp[i] = (SQRT3 - 1) / SQRT2 * interp[i];
-        }
-        for (int i = 0; i < input.length / 2; i++) {
-            diff[i] = (SQRT3 + 1) / SQRT2 * diff[i];
-        }
+            //Normalize vectors.
+            for (int i = 0; i < input.length / 2; i++) {
+                interp[i] = (SQRT3 - 1) / SQRT2 * interp[i];
+            }
+            for (int i = 0; i < input.length / 2; i++) {
+                diff[i] = (SQRT3 + 1) / SQRT2 * diff[i];
+            }
 //        System.out.println();
 //        System.out.println(Arrays.toString(interp));
 //        System.out.println(Arrays.toString(diff));
-        System.arraycopy(interp, 0, output, 0, input.length / 2);
-        System.arraycopy(diff, 0, output, input.length / 2, input.length / 2);
-
-        return output;
+            System.arraycopy(interp, 0, output, 0, input.length / 2);
+            System.arraycopy(diff, 0, output, input.length / 2, input.length / 2);
+            System.out.println("Output: "+Arrays.toString(output));
+            System.out.println();
+            
+            //Swap arrays to do next iteration
+            DaubWaveTrans(interp);
+    return null;
 
     }
-
 }

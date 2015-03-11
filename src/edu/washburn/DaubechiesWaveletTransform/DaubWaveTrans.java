5fec1fea5e2ce959375d3d2b1dba2d5130aac7f6
“r4dat” <rfourtoss@gmail.com>
2015-03-10 21:09:11 -0500

    Change var names to be follow Haar format.

--- src/edu/washburn/DaubechiesWaveletTransform/DaubWaveTrans.java
+++ src/edu/washburn/DaubechiesWaveletTransform/DaubWaveTrans.java
@@ -14,46 +14,47 @@ public class DaubWaveTrans {
 
     public static double[] DaubWaveTrans(double[] input) {
 // This function assumes that input.length=2^n, n>1
-        int size = input.length;
-        double[] interp = new double[size];
-        double[] diff = new double[size];
+        double[] output = new double[input.length];
+        double[] interp = new double[input.length/2];
+        double[] diff = new double[input.length/2];
+        
+        
+        
         //First step does interpolation between pairs of sequential elelments.
         //1,2; 2,3; 4,5 etc.
-        for (int i = 0; i < size / 2; i++) {
+        for (int i = 0; i < input.length / 2; i++) {
             interp[i] = input[2 * i] + SQRT3 * input[2 * i + 1];
         }
 
         //Calculate weighted difference between input previous 2 interp values.
         //The pre-for loop line handles index 0 by wrapping around. 
-        diff[0] = input[1] - SQRT3 / 4.0 * interp[0] - (SQRT3 - 2) / 4.0 * interp[size / 2 - 1];
-        for (int i = 1; i < size / 2; i++) {
+        diff[0] = input[1] - SQRT3 / 4.0 * interp[0] - (SQRT3 - 2) / 4.0 * interp[input.length / 2 - 1];
+        for (int i = 1; i < input.length / 2; i++) {
             diff[i] = input[2 * i + 1] - SQRT3 / 4.0 * interp[i] - (SQRT3 - 2) / 4.0 * interp[i - 1];
         }
 
         //Adjust interpolations towards the next difference value. (Correction)
-        for (int i = 0; i < size / 2 - 1; i++) {
+        for (int i = 0; i < input.length / 2 - 1; i++) {
             interp[i] = interp[i] - diff[i + 1];
         }
         //Deal with final element correct by wrapping around. 
         //Overwrites previous entry.
-        interp[size / 2 - 1] = interp[size / 2 - 1] - diff[0];
+        interp[input.length / 2 - 1] = interp[input.length / 2 - 1] - diff[0];
 
         //Normalize vectors.
-        for (int i = 0; i < size / 2; i++) {
+        for (int i = 0; i < input.length / 2; i++) {
             interp[i] = (SQRT3 - 1) / SQRT2 * interp[i];
         }
-        for (int i = 0; i < size / 2; i++) {
+        for (int i = 0; i < input.length / 2; i++) {
             diff[i] = (SQRT3 + 1) / SQRT2 * diff[i];
         }
-        System.out.println();
-        System.out.println(Arrays.toString(interp));
-        System.out.println(Arrays.toString(diff));
-        double[] retarr = new double[size];
-        System.arraycopy(interp, 0, retarr, 0, size / 2);
-        System.arraycopy(diff, 0, retarr, size / 2, size / 2);
-        System.out.println(Arrays.toString(retarr));
+//        System.out.println();
+//        System.out.println(Arrays.toString(interp));
+//        System.out.println(Arrays.toString(diff));
+        System.arraycopy(interp, 0, output, 0, input.length / 2);
+        System.arraycopy(diff, 0, output, input.length / 2, input.length / 2);
 
-        return retarr;
+        return output;
 
     }
 
